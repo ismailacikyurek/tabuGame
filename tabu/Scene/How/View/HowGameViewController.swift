f18c45bacf2d3c9ab6dacf1be0e633979f9e33e8
ismailacikyurek <ismailackyurek@gmail.com>
2023-06-26 17:41:00 +0300

    howVC add

---
+++ tabu/Scene/How/View/HowGameViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HowGameViewController.swift
+//  tabu
+//
+//  Created by İSMAİL AÇIKYÜREK on 26.06.2023.
+//
+
+import UIKit
+
+class HowGameViewController: UIViewController {
+
+  @IBOutlet weak var mainView: UIView!
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+    mainView.layer.masksToBounds = true
+    mainView.layer.cornerRadius = 20
+    }
+
+
+  @IBAction func exitButtonTapped(_ sender: Any) {
+    dismiss(animated: true)
+  }
+
+
+}
