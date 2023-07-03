0fbc2bcc53be8100b12b60411ee4d5c7ffd2e090
ismailacikyurek <ismailackyurek@gmail.com>
2023-07-03 14:25:17 +0300

    alertMessage class edit commit

--- tabu/Helperr/AlertMessage.swift
+++ tabu/Helperr/AlertMessage.swift
@@ -2,21 +2,18 @@
 //  AlertMessage.swift
 //  pokemonCase
 //
-//  Created by İSMAİL AÇIKYÜREK on 27.03.2023.
+//  Created by İSMAİL AÇIKYÜREK on 27.06.2023.
 //
 
 import UIKit
     
 extension UIViewController {
-     func alertMessageShow(title : successOrError,message : String) {
-        let alert = UIAlertController(title: title.rawValue, message: message, preferredStyle: .alert)
-       let ok = UIAlertAction(title: "Ok", style: .default)
+     func alertMessageShow(title : String,message : String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "Ok", style: .default)
         alert.addAction(ok)
         present(alert, animated: true)
     }
 }
 
-enum successOrError : String {
-    case error = "Error"
-    case success = "Success"
-}
+
