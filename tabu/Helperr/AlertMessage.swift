fdbb7524ec5841942014420bfc1124d69e91608d
ismailacikyurek <ismailackyurek@gmail.com>
2023-07-03 23:01:36 +0300

    alertMessage commit

--- tabu/Helperr/AlertMessage.swift
+++ tabu/Helperr/AlertMessage.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
-    
+
 extension UIViewController {
-     func alertMessageShow(title : String,message : String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let ok = UIAlertAction(title: "Ok", style: .default)
-        alert.addAction(ok)
-        present(alert, animated: true)
+  func alertMessageShow(title : String,message : String) {
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let ok = UIAlertAction(title: "Tamam", style: .default)
+    alert.addAction(ok)
+    present(alert, animated: true)
+  }
+
+  func showAlertWithTwoCustomAction(vc: UIViewController, title: String, message: String, buttonTitle: String, completion: (() -> Void)? = nil) {
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let addAction = UIAlertAction(title: buttonTitle, style: .default) { action in
+      if let completion = completion {
+        completion()
+      }
     }
+    alert.addAction(addAction)
+    vc.present(alert, animated: true, completion: nil)
+  }
 }
+
 
 
